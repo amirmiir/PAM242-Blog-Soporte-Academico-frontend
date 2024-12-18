80a912513dcf25a01f35f1853f593cf7f5287108
Janiopi <[janio072003@hotmail.com]>
2024-12-18 11:14:51 -0500

    Testing using fetch to make request to backend

--- frontend-blog/src/pages/recover-password/RecoverForm.tsx
+++ frontend-blog/src/pages/recover-password/RecoverForm.tsx
@@ -3,9 +3,119 @@ import React from 'react'
 type Props = {}
 
 const RecoverForm = (props: Props) => {
+    const [message, setMessage] = useState<string>('');
+   
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Inputs>();
+
+    const onSubmit: SubmitHandler<Inputs> = async (formData) => {
+        console.log('Form Data:', formData);
+
+        try {
+            const response = await fetch('http://localhost:4000/users/login', {  //fetch es una API para realizar solicitudes HTTP
+            
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (!response.ok) {
+                const error = await response.json(); 
+                throw new Error(error.message||'Login failed');
+            }
+
+            //Se logeo exitosamente
+            const responseData = await response.json();
+            console.log(responseData);
+            
+            setMessage('Login successful!');
+            
+        } catch (error: any) {
+            setMessage(error.message || 'An error occurred');
+        }
+    };
+
+    const handleGoogleSignIn = () => {
+
+        window.location.href = 'http://localhost:4000/users/authGoogle';
+        //¿Por que no fetch? La autenticacion por Google se basa en un redireccionamiento hacia la pagina de logeo de Google. No es una API con req-res
+
+        console.log('Google Sign-In not implemented yet.');
+    };
+
     return (
-        <div>RecoverForm</div>
-    )
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)} id="login-form">
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="email"
+                    >
+                        Email
+                    </label>
+                    <input
+                        {...register('email', { required: true })}
+                        type="email"
+                        id="email"
+                        placeholder="Email Address"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                    />
+                    {errors.email && (
+                        <p className="text-red-500 text-xs italic">
+                            Email is required.
+                        </p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password"
+                    >
+                        Contraseña
+                    </label>
+                    <input
+                        {...register('password', { required: true })}
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                    />
+                    {errors.password && (
+                        <p className="text-red-500 text-xs italic">
+                            Password is required.
+                        </p>
+                    )}
+                </div>
+                {message && (
+                    <p className="text-red-500 text-xs italic mb-3">{message}</p>
+                )}
+                <div>
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none"
+                    >
+                        Login
+                    </button>
+                </div>
+            </form>
+
+            {/* google sign in */}
+            <div className="mt-4">
+                <button
+                    onClick={handleGoogleSignIn}
+                    className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+                >
+                        <FaGoogle className="mr-2" />
+                    Iniciar Sesión con Google
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default RecoverForm
--- frontend-blog/src/pages/register/RegisterForm.tsx
+++ frontend-blog/src/pages/register/RegisterForm.tsx
@@ -1,9 +1,169 @@
-import { FC } from 'react'
+import React, { FC, useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type Inputs = {
+    name: string;
+    email: string;
+    password: string;
+    password2: string;
+};
+
+
 
 const RegisterForm: FC = () => {
+        const [message, setMessage] = useState<string>('')
+        const [backendErrors,setBackendErrors] = useState<string[]>([]);
+    
+        const {
+            register,
+            handleSubmit,
+            formState: { errors },
+        } = useForm<Inputs>();
+
+        const onSubmit: SubmitHandler<Inputs> = async (formData) => {
+            console.log('Form Data:', formData);
+            
+            try {
+                const response = await fetch('http://localhost:4000/users/register', {  //fetch es una API para realizar solicitudes HTTP
+                
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formData),
+                });
+
+                if (!response.ok) {
+                    const errorData = await response.json(); 
+                    setBackendErrors(errorData.errors.map((err: { message: string }) => err.message)); // Store backend errors
+                    throw new Error(errorData.message||'Register failed');
+                }
+
+                //Se registro exitosamente
+                const responseData = await response.json();
+                console.log(responseData);
+                
+                setMessage('Register successful!');
+                setBackendErrors([])
+                
+            } catch (error: any) {
+                setMessage(error.message || 'An error occurred');
+            }
+        };
+
+
     return (
-        <div>RegisterForm</div>
-    )
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)} id="register-form">
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="name"
+                    >
+                        Name
+                    </label>
+                    <input
+                        {...register('name', { required: true })}
+                        type="name"
+                        id="name"
+                        placeholder="UserName"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                    />
+                    {errors.name && (
+                        <p className="text-red-500 text-xs italic">
+                            Username is required.
+                        </p>
+                    )}
+                </div>
+             
+             
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="email"
+                    >
+                        Email
+                    </label>
+                    <input
+                        {...register('email', { required: true })}
+                        type="email"
+                        id="email"
+                        placeholder="Email Address"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                    />
+                    {errors.email && (
+                        <p className="text-red-500 text-xs italic">
+                            Email is required.
+                        </p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password"
+                    >
+                        Contraseña
+                    </label>
+                    <input
+                        {...register('password', { required: true })}
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                    />
+                    {errors.password && (
+                        <p className="text-red-500 text-xs italic">
+                            Password is required.
+                        </p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor="password2"
+                    >
+                        Contraseña
+                    </label>
+                    <input
+                        {...register('password2', { required: true })}
+                        type="password"
+                        id="password2"
+                        placeholder="Confirm password"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+                    />
+                    {errors.password2 && (
+                        <p className="text-red-500 text-xs italic">
+                            Password is required.
+                        </p>
+                    )}
+                </div>
+
+                {backendErrors.length > 0 && (
+                <div className="text-red-500 text-xs italic">
+                    {backendErrors.map((error, index) => (
+                        <p key={index}>{error}</p>
+                    ))}
+                </div>
+                )}
+    
+
+
+                {message && (
+                    <p className="text-red-500 text-xs italic mb-3">{message}</p>
+                )}
+                <div>
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none"
+                    >
+                        Register
+                    </button>
+                </div>
+            </form>
+
+         
+        </div>
+    );
 }
 
 export default RegisterForm
