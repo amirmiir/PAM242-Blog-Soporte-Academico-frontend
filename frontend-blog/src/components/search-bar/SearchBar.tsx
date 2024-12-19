1576c7dfe84cad2ab43b5eeaf1b2d7927180694a
amirmiir <00aamircg@gmail.com>
2024-12-18 19:26:39 -0500

    feat: searchBar modified for proper work when written and immediate update

--- frontend-blog/src/components/search-bar/SearchBar.tsx
+++ frontend-blog/src/components/search-bar/SearchBar.tsx
@@ -3,6 +3,7 @@ import { IoSearchOutline } from 'react-icons/io5'
 
 type SearchBar = {
     'placeholder': string
+    onSearchChange?: (text:string) => void
 }
 
 type SearchBarProps = {
@@ -10,10 +11,16 @@ type SearchBarProps = {
 }
 
 const SearchBar: FC<SearchBarProps> = ({ searchBar }) => {
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (searchBar.onSearchChange) {
+            searchBar.onSearchChange(event.target.value); // Notify parent of input change
+        }
+    };
+
     return (
         <div className="relative sm:w-72 w-40 space-x-2">
             <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
-            <input type="text" placeholder={searchBar.placeholder} className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounder-md focus:outline-none" />
+            <input type="text" placeholder={searchBar.placeholder} className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounder-md focus:outline-none" onChange={handleInputChange}/>
         </div>
     )
 }
--- frontend-blog/src/pages/subjects/Subjects.tsx
+++ frontend-blog/src/pages/subjects/Subjects.tsx
@@ -1,20 +1,27 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import NavBar from '../../components/nav-bar/NavBar'
 import SubjectsContent from './SubjectsContent'
 import Footer from '../../components/footer/Footer'
 import SearchBar from '../../components/search-bar/SearchBar'
 
 const Subjects: FC = () => {
+    /** hook to look after changes on searchbar */
+    const [searchText, setSearchText] = useState<string>(''); // State to track input
+
+    const handleSearchChange = (text: string) => {
+        setSearchText(text); // Update state on input change
+    };
+
     return (
         <div className="h-[calc(100vh-10rem)]">
             <NavBar />
             <div className="flex flex-col items-center py-8 space-y-4">
                 <h1 className="text-3xl font-semibold">Explora los cursos</h1>
                 <p className="text-xl">Busca cursos, materiales y fuentes de enseñanza</p>
-                <SearchBar searchBar={{ placeholder: 'Buscar materias y recursos' }} />
+                <SearchBar searchBar={{ placeholder: 'Buscar materias y recursos', onSearchChange: handleSearchChange }} />
             </div>
 
-            <SubjectsContent search=""/>
+            <SubjectsContent search={searchText}/>
             
             <Footer />
         </div>
